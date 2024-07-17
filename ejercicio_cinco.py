2acc519c937e162d4bc89e84e142f4aab0441afd
jesscabna <githubcuenta776@gmail.com>
2024-07-17 10:56:22 -0500

    ejercicio 5  profesor

--- ejercicio_cinco.py
+++ ejercicio_cinco.py
@@ -4,3 +4,16 @@ ejm:
 entrada: ("jose","alvarez","30","APSTI","III")
 salida: {nombre:"jose",apellido:"alvarez",edad:"30",programa_estudio:"APSTI",semestre:"III"}
 """
+
+def datos(nombre, apellido, edad, programa_estudio, semestre):
+
+    return {
+        'nombre': nombre,
+        'apellido': apellido,
+        'edad': edad,
+        'programa_estudio': programa_estudio,
+        'semestre': semestre
+    }
+
+resultado = datos("catalina", "A.", "19", "APSTI", "III")
+print(resultado)
