173cfc2d4b49f1c9d36a2a3091fe8fdf68fcbce9
jesscabna <githubcuenta776@gmail.com>
2024-07-17 10:45:42 -0500

    ejercicio 1  profesor

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,21 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+def encontrar_menor_mayor(lista):
+    menor = lista[0]
+    mayor = lista[0]
+
+    for num in lista:
+        if num < menor:
+            menor = num
+        if num > mayor:
+            mayor = num
+
+    diccionario = {'menor': menor, 'mayor': mayor}
+
+    return diccionario
+
+# Ejemplo de uso
+entrada = [4, 7,8,12,14,20,100]
+salida = encontrar_menor_mayor(entrada)
+print(salida)
