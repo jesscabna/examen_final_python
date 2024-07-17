6ce5d666c8f32a391683551567229cf795aad6f6
jesscabna <githubcuenta776@gmail.com>
2024-07-17 11:00:29 -0500

    ejercicio 3 profesor

--- ejercicio_tres.py
+++ ejercicio_tres.py
@@ -4,3 +4,11 @@ ejm:
 entrada: eucalipto
 salida: {e:1,u:1,a:1,i:1,o:1}
 """
+def vocales(texto):
+
+    vocales = {'a', 'e', 'i', 'o', 'u'}
+    resultado = {v: texto.lower().count(v) for v in vocales}
+    return resultado
+
+resultado = vocales("eucalipto")
+print(resultado)
